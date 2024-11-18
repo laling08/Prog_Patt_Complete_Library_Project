c03e1ba8827c221d15327fcd41492867ee1b7560
laling08 <ishiliagilcedeslabrador@gmail.com>
2024-11-18 12:36:45 -0500

    started gui logic

--- src/main/java/org/example/CompleteLibrary.java
+++ src/main/java/org/example/CompleteLibrary.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class CompleteLibrary {
+import javax.swing.*;
+
+public class CompleteLibrary extends JFrame {
+
+    public CompleteLibrary () {
+
+        setTitle ("Complete Library App"); // will be the title of the window
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // when you close you window, it will exit your project
+        setSize(700, 500);
+        setLocationRelativeTo(null); //
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CompleteLibrary();
+    }
 }
