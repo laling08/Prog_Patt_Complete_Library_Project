ef06cbfe19539d03782cede6184f90ef7e6e08e3
Ishilia <ishiliagilcedeslabrador@gmail.com>
2024-11-18 21:55:08 -0500

    added the components of CompleteLibrary.form, and added empty event listeners to be populated later

--- src/main/java/org/example/CompleteLibrary.java
+++ src/main/java/org/example/CompleteLibrary.java
@@ -1,16 +1,31 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CompleteLibrary extends JFrame {
-
     private JLabel welcomeLB;
+    private JButton librarianBT;
+    private JButton memberBT;
 
     public CompleteLibrary () {
         setTitle("Complete Library App"); // this will be the title of the window
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(700,500);
         setLocationRelativeTo(null);
+        librarianBT.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // add the even listener here for librarian button
+            }
+        });
+        memberBT.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // add the event listener for member button
+            }
+        });
     }
 
     public static void main(String[] args) {
